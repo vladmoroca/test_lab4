153a38c715835a171645ff4475d7c6dd64399255
vladmoroca <vladmoroca@gmail.com>
2023-11-28 17:04:25 +0200

    jest mocks

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,44 +1,39 @@
 import { Main } from "../src/main.js";
+import { fileSystem } from "../src/fileSystem.js";
+import { Game } from "../src/tetris.js"
 
+jest.mock("../src/fileSystem.js", () => {
+    return {
+        fileSystem: jest.fn().mockImplementation(function(args) {
+            this.args = args;
+            return {
+                readArgs: jest.fn(() => this.args),
+                parse: jest.fn((args) => args),
+                writeOutput: jest.fn((args) => args),
+            };
+        }),
+    };
+});
 
-class FileSystemMock {
-    constructor(args){
-        this.args = args
-    }
-    readArgs(){
-        return this.args
-    }
-    parse(args) {
-      return args
-    }
+jest.mock("../src/tetris.js", () => {
+    return {
+        Game: jest.fn().mockImplementation(function() {
+            this.data = [];
+            return {
+                init: jest.fn( data => this.data = data),
+                getField: jest.fn(() => this.data),
+                run: jest.fn(() => this.data.reverse()),
+            };
+        }),
+    };
+});
 
-    writeOutput(data){
-        return data
-    }
-}
-
-class GameMock {
-    constructor(){
-        this.data = []
-    }
-    init(data){
-        this.data = data
-    }
-
-    getField(){
-        return this.data
-    }
-
-    run(){
-        this.data = this.data.reverse()
-    }
-}
 
 describe("use mock", () => {
     let fs, game, main;
     beforeEach(() => {
-        fs = new FileSystemMock(['7', '', '8'])
-        game = new GameMock()
+        fs = new fileSystem(['7', '', '8'])
+        game = new Game()
         main = new Main(fs, game)
     });
     
